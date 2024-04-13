ccafca8f78f3fa12f15f2c2425322b11e0fc352d
Thomas <segfault.github@gmail.com>
2024-04-13 11:14:22 +0200

    Add resources

---
+++ Assets/Code/Utils/WeightDistribution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Utils {
+    [Serializable]
+    public struct WeightDistribution<T> {
+        #region Members
+        [SerializeField] private float m_Weight;
+        [SerializeField] private T m_Obj;
+        #endregion
+
+        #region Getters / Setters
+        public float Weight { get => this.m_Weight; }
+        public T Obj { get => this.m_Obj; }
+        #endregion
+    }
+}
