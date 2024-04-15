396fe2344ee76a3cbc98b93fa4313beeefa059f5
Deloriand <deloprod@gmail.com>
2024-04-15 12:31:13 +0200

    Ingame menu ui

--- Assets/Code/UI/InGameMenu.cs
+++ Assets/Code/UI/InGameMenu.cs
@@ -5,16 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour
 {
+
+    public GameObject previousLevelButton;
+    public GameObject nextLevelButton;
+    public GameObject endGameMenu;
+
+    private int currentSceneId;
+    private int numberOfScenes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        endGameMenu.SetActive(false);
+
+        currentSceneId = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneId == 1)
+        {
+            previousLevelButton.SetActive(false);
+        }
+
+        numberOfScenes = SceneManager.sceneCountInBuildSettings;
+
+        if (currentSceneId == numberOfScenes - 1)
+        {
+            nextLevelButton.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayEndGameMenu()
     {
-        
+        endGameMenu.SetActive(true);
     }
 
     public void BackToMainMenu()
@@ -25,6 +45,18 @@ public class InGameMenu : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void PreviousLevelLoad()
+    {
+        int currentSceneId = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneId - 1);
+    }
+
+    public void NextLevelLoad()
+    {
+        int currentSceneId = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneId + 1);
     }
 
     public void QuitGame()
